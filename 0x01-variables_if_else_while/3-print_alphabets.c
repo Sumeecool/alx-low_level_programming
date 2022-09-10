298fb3aef451cc9a3ee2b19006dcff01b7b4dc55
Sumeecool <sanisumayyah11@gmail.com>
2022-09-09 19:05:52 -0700

    3print

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,15 +2,14 @@
 #include <stdio.h>
 #include <time.h>
 /**
-* *main- entry point
-* *Description: Alphabets
-* *Return: zero on success
-*/
+ * *main- entry point
+ * *Description: Alphabets
+ * *Return: zero on success
+ */
 int main(void)
 {
-<<<<<<< HEAD
 	char letter = 'a';
-
+	
 	while (letter <= 'z')
 	{
 		putchar(letter);
@@ -20,26 +19,10 @@ int main(void)
 	letter = 'A';
 
 	while (letter <= 'Z')
-{
-	putchar(letter);
-	letter++;
-=======
-char letter = 'a';
-
-while (letter <= 'z')
-{
-putchar(letter);
-letter++;
-}
-
-letter = 'A';
-
-while (letter <= 'Z')
-{
-putchar(letter);
-letter++;
->>>>>>> 34630e8d38eb5747f4ee2ac0865c8f8953b37905
-}
-putchar('\n');
-return (0);
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
 }
