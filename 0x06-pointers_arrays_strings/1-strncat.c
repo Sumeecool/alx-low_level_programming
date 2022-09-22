9392e9bd0321813732098c89c7faa1ad8788eb2a
Sumeecool <sanisumayyah11@gmail.com>
2022-09-21 23:25:01 -0700

    1strncatc

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,8 +1,8 @@
 
-#include "main.h"#include "main.h"
+#include "main.h"
 
 /**
- * _strncat - concatenates two strings
+ * _strncat - concatenates two strings.
  * @dest: destination
  * @src: source
  * @n: amount of bytes used from src.
