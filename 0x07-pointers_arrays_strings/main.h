fff59994520edf1414826985504dc4368ee253cf
Sumeecool <sanisumayyah11@gmail.com>
2022-09-27 12:10:25 -0700

    hot fix

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,12 +2,14 @@
 #define MAIN_H
 
 int _putchar(char c);
-*_memset(char *s, char b, unsigned int n);
-*_memcpy(char *dest, char *src, unsigned int n);
-*_strchr(char *s, char c);
-nsigned int _strspn(char *s, char *accept);
-*_strpbrk(char *s, char *accept);
-*_strstr(char *haystack, char *needle);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 void print_diagsums(int *a, int size);
 void print_chessboard(char (*a)[8]);
 void set_string(char **s, char *to);
+
+#endif
