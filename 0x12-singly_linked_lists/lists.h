b1c4a62bc37cd6f52eb6ae927a3a3c31e8d2113e
Sumeecool <sanisumayyah11@gmail.com>
2022-10-20 20:57:58 -0700

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,13 +6,13 @@
 #include <string.h>
 
 /**
- *  * struct list_s - singly linked list
- *   * @str: string - (malloc'ed string)
- *    * @len: length of the string
- *     * @next: points to the next node
- *      * Description: singly linked list node structure
- *       * for Holberton project
- *        */
+* struct list_s - singly linked list
+* @str: string - (malloc'ed string)
+* @len: length of the string
+* @next: points to the next node
+* Description: singly linked list node structure
+* for Holberton project
+*/
 
 typedef struct list_s
 {
